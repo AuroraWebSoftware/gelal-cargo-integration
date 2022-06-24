e7c471d868ca3fbbe585a7daae3d65d1583abfee
Mehmet Akif TEZCAN <akif.tezcan@aurorabilisim.com>
2022-06-24 23:23:41 +0300

    #second commit

--- src/IntegrationTrait.php
+++ src/IntegrationTrait.php
@@ -1,11 +1,12 @@
 <?php
 
-class IntegrationTrait
+class GelalApiTrait
 {
     public function __construct()
     {
 
     }
+
     public function send(){
         print_r("Hellooo");
         die();
